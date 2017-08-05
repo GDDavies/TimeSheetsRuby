07cb7eb5716b9177993df072b11ef32d8e97ef01
George Davies <georgedavies@iMac.local>
2017-08-05 12:03:36 +0100

    Some validation

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,5 @@
 class Project < ApplicationRecord
+  validates :project_name, length: { maximum: 50 }, presence: true
+  validates :project_code, length: { maximum: 7 }, presence: true
+  validates :gate_passed, :inclusion => 0..5, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    validates :first_name, length: { maximum: 50 }, presence: true
+    validates :last_name, length: { maximum: 50 }, presence: true
+    validates :email, length: { maximum: 50 }, presence: true
+    validates :job_title, length: { maximum: 30 }
 end
