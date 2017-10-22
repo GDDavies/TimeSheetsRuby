a651a925a34e7a57a77b94637c5191ac460026a5
George <gdaviesdev@gmail.com>
2017-10-22 18:45:28 +0100

    Implement advanced login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171022104655) do
+ActiveRecord::Schema.define(version: 20171022164319) do
 
   create_table "projects", force: :cascade do |t|
     t.string "project_name"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20171022104655) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
