e1a2172d649cbd008d3858dcc97862e51c4fee90
George <gdaviesdev@gmail.com>
2017-10-22 14:33:00 +0100

    Changed Puma config

--- config/puma.rb
+++ config/puma.rb
@@ -47,7 +47,7 @@ threads threads_count, threads_count
 preload_app!
 
 rackup      DefaultRackup
-port        ENV['PORT']     || 3030
+port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
